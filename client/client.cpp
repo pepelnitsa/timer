8a62801da219c0c97033157bcb936bfedc9afc96
pepelnitsa <47673744+pepelnitsa@users.noreply.github.com>
2024-04-12 12:49:10 +0300

    Добавьте файлы проекта.

---
+++ client/client.cpp
@@ -0,0 +1,354 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <conio.h>
+#include <string>
+#include <thread>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+#define PAUSE 1
+
+SOCKET ConnectSocket = INVALID_SOCKET;
+
+COORD server_smile;
+COORD client_smile;
+
+enum class KeyCodes { LEFT = 75, RIGHT = 77, UP = 72, DOWN = 80, ENTER = 13, ESCAPE = 27, SPACE = 32, ARROWS = 224 };
+enum class Colors { BLUE = 9, RED = 12, BLACK = 0, YELLOW = 14, DARKGREEN = 2 };
+
+bool gameRunning = true;
+int elapsedSeconds = 0;
+
+struct Coin {
+    int x;
+    int y;
+    bool taken;
+};
+
+vector<Coin> server_coins;
+vector<Coin> client_coins;
+int server_coin_count = 0;
+int client_coin_count = 0;
+
+DWORD WINAPI TimerThread(LPVOID lpParam) {
+    while (gameRunning) {
+        this_thread::sleep_for(chrono::seconds(1));
+        elapsedSeconds++;
+
+        int hours = elapsedSeconds / 3600;
+        int minutes = (elapsedSeconds % 3600) / 60;
+        int seconds = elapsedSeconds % 60;
+
+        string title = "Elapsed Time: ";
+        title += to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+        wstring wideTitle(title.begin(), title.end());
+
+        SetConsoleTitle(wideTitle.c_str());
+
+        if (minutes >= 1) {
+            system("cls");
+            cout << "SERVER: " << server_coin_count << endl;
+            cout << "CLIENT: " << client_coin_count << endl;
+            gameRunning = false;
+        }
+    }
+
+    return 0;
+}
+
+DWORD WINAPI Sender(void* param)
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_CURSOR_INFO cursor;
+    cursor.bVisible = false;
+    cursor.dwSize = 100;
+    SetConsoleCursorInfo(h, &cursor);
+
+    while (true)
+    {
+        KeyCodes code = (KeyCodes)_getch();
+        if (code == KeyCodes::ARROWS) code = (KeyCodes)_getch();
+
+        SetConsoleCursorPosition(h, client_smile);
+        cout << " ";
+
+        int direction = 0;
+
+        if (code == KeyCodes::LEFT) {
+            client_smile.X--;
+            direction = 1;
+        }
+        else if (code == KeyCodes::RIGHT) {
+            client_smile.X++;
+            direction = 2;
+        }
+        else if (code == KeyCodes::UP) {
+            client_smile.Y--;
+            direction = 3;
+        }
+        else if (code == KeyCodes::DOWN) {
+            client_smile.Y++;
+            direction = 4;
+        }
+
+        SetConsoleCursorPosition(h, client_smile);
+        SetConsoleTextAttribute(h, WORD(Colors::BLUE));
+        cout << (char)1;
+
+        string coinPositions = "";
+        for (const Coin& coin : client_coins) {
+            coinPositions += to_string(coin.x) + "," + to_string(coin.y) + ";";
+        }
+        char message[200];
+        strcpy_s(message, 199, (to_string(direction) + ";" + coinPositions).c_str());
+
+        int iResult = send(ConnectSocket, message, (int)strlen(message), 0);
+        if (iResult == SOCKET_ERROR) {
+            cout << "send failed with error: " << WSAGetLastError() << "\n";
+            closesocket(ConnectSocket);
+            WSACleanup();
+            return 15;
+        }
+    }
+
+    return 0;
+}
+
+void ParseData(char data[], char**& map, unsigned int& rows, unsigned int& cols)
+{
+    string info = data;
+    int h_index = info.find("h");
+    int w_index = info.find("w");
+    int d_index = info.find("d");
+    string r = "";
+    if (w_index == 2) r = info.substr(1, 1);
+    else r = info.substr(1, 2);
+    int rows_count = stoi(r);
+    int distance = d_index - w_index - 1;
+    string columns = info.substr(w_index + 1, distance);
+    int columns_count = stoi(columns);
+    string map_data = info.substr(d_index + 1, rows_count * columns_count);
+
+    rows = rows_count;
+    cols = columns_count;
+    int map_data_current_element = 0;
+    map = new char* [rows_count];
+    for (int y = 0; y < rows_count; y++)
+    {
+        map[y] = new char[columns_count];
+        for (int x = 0; x < columns_count; x++)
+        {
+            map[y][x] = map_data[map_data_current_element];
+            map_data_current_element++;
+        }
+    }
+}
+
+void ShowMap(char** map, const unsigned int height, const unsigned int width) {
+    setlocale(0, "C");
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            bool coin_displayed = false;
+            for (const Coin& coin : server_coins) {
+                if (coin.x == x && coin.y == y && !coin.taken) {
+                    SetConsoleTextAttribute(h, WORD(Colors::YELLOW));
+                    cout << "o";
+                    coin_displayed = true;
+                    break;
+                }
+            }
+            if (coin_displayed) continue;
+
+            for (const Coin& coin : client_coins) {
+                if (coin.x == x && coin.y == y && !coin.taken) {
+                    SetConsoleTextAttribute(h, WORD(Colors::YELLOW));
+                    cout << "o";
+                    coin_displayed = true;
+                    break;
+                }
+            }
+            if (coin_displayed) continue;
+
+            if (map[y][x] == ' ')
+            {
+                SetConsoleTextAttribute(h, WORD(Colors::BLACK));
+                cout << " ";
+            }
+            else if (map[y][x] == '#')
+            {
+                SetConsoleTextAttribute(h, WORD(Colors::DARKGREEN));
+                cout << (char)219;
+            }
+            else if (map[y][x] == '1')
+            {
+                SetConsoleTextAttribute(h, WORD(Colors::RED));
+                cout << (char)1;
+                server_smile.X = x;
+                server_smile.Y = y;
+            }
+            else if (map[y][x] == '2')
+            {
+                SetConsoleTextAttribute(h, WORD(Colors::BLUE));
+                cout << (char)1;
+                client_smile.X = x;
+                client_smile.Y = y;
+            }
+        }
+        cout << "\n";
+    }
+}
+
+void CheckCoinCollision() {
+    for (Coin& client_coin : client_coins) {
+        if (client_coin.x == server_smile.X && client_coin.y == server_smile.Y && !client_coin.taken) {
+            client_coin.taken = true;
+            client_coin_count++;
+        }
+    }
+
+    for (Coin& server_coin : server_coins) {
+        if (server_coin.x == client_smile.X && server_coin.y == client_smile.Y && !server_coin.taken) {
+            server_coin.taken = true;
+            server_coin_count++;
+        }
+    }
+}
+
+DWORD WINAPI Receiver(void* param)
+{
+    char answer[DEFAULT_BUFLEN];
+    int iResult = recv(ConnectSocket, answer, DEFAULT_BUFLEN, 0);
+    answer[iResult] = '\0';
+
+    char** map = nullptr;
+    unsigned int rows;
+    unsigned int cols;
+    ParseData(answer, map, rows, cols);
+    ShowMap(map, rows, cols);
+
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    while (true)
+    {
+        int iResult = recv(ConnectSocket, answer, DEFAULT_BUFLEN, 0);
+        answer[iResult] = '\0';
+
+        string message = answer;
+
+        SetConsoleCursorPosition(h, server_smile);
+        SetConsoleTextAttribute(h, WORD(Colors::BLACK));
+        cout << " ";
+
+        if (message == "1") {
+            server_smile.X--;
+        }
+        else if (message == "2") {
+            server_smile.X++;
+        }
+        else if (message == "3") {
+            server_smile.Y--;
+        }
+        else if (message == "4") {
+            server_smile.Y++;
+        }
+
+        SetConsoleCursorPosition(h, server_smile);
+        SetConsoleTextAttribute(h, WORD(Colors::RED));
+        cout << (char)1;
+
+        if (iResult > 0) {
+            CheckCoinCollision();
+        }
+        else if (iResult == 0)
+            cout << "соединение с сервером закрыто.\n";
+        else
+            cout << "recv failed with error: " << WSAGetLastError() << "\n";
+    }
+    return 0;
+}
+
+int main()
+{
+    setlocale(0, "");
+    system("title CLIENT SIDE");
+    MoveWindow(GetConsoleWindow(), 50, 50, 500, 500, true);
+
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        cout << "WSAStartup failed with error: " << iResult << "\n";
+        return 11;
+    }
+
+    addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    const char* ip = "localhost";
+    addrinfo* result = NULL;
+    iResult = getaddrinfo(ip, DEFAULT_PORT, &hints, &result);
+
+    if (iResult != 0) {
+        cout << "getaddrinfo failed with error: " << iResult << "\n";
+        WSACleanup();
+        return 12;
+    }
+    else {
+        Sleep(PAUSE);
+    }
+
+    for (addrinfo* ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+        if (ConnectSocket == INVALID_SOCKET) {
+            cout << "socket failed with error: " << WSAGetLastError() << "\n";
+            WSACleanup();
+            return 13;
+        }
+
+        iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        cout << "невозможно подключиться к серверу!\n";
+        WSACleanup();
+        return 14;
+    }
+
+    server_coins.push_back({ 3, 4, false });
+    server_coins.push_back({ 7, 2, false });
+    server_coins.push_back({ 8, 5, false });
+    server_coins.push_back({ 6, 8, false });
+    server_coins.push_back({ 1, 9, false });
+
+    CreateThread(0, 0, Sender, 0, 0, 0);
+    CreateThread(0, 0, Receiver, 0, 0, 0);
+    CreateThread(0, 0, TimerThread, 0, 0, 0);
+    Sleep(INFINITE);
+}
---
+++ smile1/server.cpp
@@ -0,0 +1,250 @@
+﻿#define WIN32_LEAN_AND_MEAN // To speed the build process: https://stackoverflow.com/questions/11040133/what-does-defining-win32-lean-and-mean-exclude-exactly
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <ws2tcpip.h> // WSADATA type; WSAStartup, WSACleanup functions and many more
+using namespace std;
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015" // a port is a logical construct that identifies a specific process or a type of network service - https://en.wikipedia.org/wiki/Port_(computer_networking)
+
+#define PAUSE 1
+
+// Accept a client socket
+SOCKET ClientSocket = INVALID_SOCKET;
+
+void GenerateMap(char map[][9], const unsigned int height, const unsigned int width) {
+	for (int y = 0; y < height; y++) // перебiр рядкiв
+	{
+		for (int x = 0; x < width; x++) // перебiр кожноi комiрки в межах рядка
+		{
+			map[y][x] = ' '; // за замовчуванням всi комiрки - це коридори 
+			if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+				map[y][x] = '#'; // стiнка
+			if (x == width - 1 && y == height / 2)
+				map[y][x] = ' '; // вихiд 
+			if (x == 1 && y == 1)
+				map[y][x] = '1'; // гравець сервера
+			if (x == 1 && y == 3)
+				map[y][x] = '2'; // гравець клiента
+		}
+	}
+}
+
+void ShowMap(char map[][9], const unsigned int height, const unsigned int width) {
+	setlocale(0, "C");
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	for (int y = 0; y < height; y++) // перебiр рядкiв
+	{
+		for (int x = 0; x < width; x++) // перебiр кожноi комiрки в межах рядка
+		{
+			if (map[y][x] == ' ')
+			{
+				SetConsoleTextAttribute(h, 0);
+				cout << " ";
+			}
+			else if (map[y][x] == '#')
+			{
+				SetConsoleTextAttribute(h, 2);
+				cout << (char)219; // cout << "#"; 
+			}
+			else if (map[y][x] == '1')
+			{
+				SetConsoleTextAttribute(h, 12);
+				cout << (char)1; // cout << "@";
+			}
+			else if (map[y][x] == '2')
+			{
+				SetConsoleTextAttribute(h, 9);
+				cout << (char)1; // cout << "@";
+			}
+		}
+		cout << "\n"; // перехiд на наступний рядок
+	}
+}
+
+string MakeMessage(char map[][9], const unsigned int height, const unsigned int width)
+{
+	string message = "h";
+	message += to_string(height); // h5
+	message += "w"; // h5w
+	message += to_string(width); // h5w9
+	message += "d"; // d = data
+	for (int y = 0; y < height; y++)
+		for (int x = 0; x < width; x++)
+			message += map[y][x];
+	return message;
+
+	/*string message; // "h5w9data#########         @@@"
+	message = "h"; // h
+	message = message + to_string(height); // h5
+	message = message + "w"; // h5w
+	message = message + to_string(width); // h5w9
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			message = message + map[y][x];
+		}
+	}*/
+}
+
+DWORD WINAPI Sender(void* param)
+{
+	// формування локацii
+	const unsigned int height = 5; // <<< висота рiвня в рядках
+	const unsigned int width = 9; // <<< ширина рiвня в стовпчиках
+	char map[height][width] = {}; // статичний масив для зберiгання всiх об'ектiв локацii
+	GenerateMap(map, height, width);
+	system("cls");
+	ShowMap(map, height, width);
+
+	string message = MakeMessage(map, height, width);
+	// cout << message << "\n";
+
+	int iSendResult = send(ClientSocket, message.c_str(), message.size(), 0); // The send function sends data on a connected socket: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-send
+	if (iSendResult == SOCKET_ERROR) {
+		cout << "send failed with error: " << WSAGetLastError() << "\n";
+		cout << "упс, отправка (send) ответного сообщения не состоялась ((\n";
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 7;
+	}
+
+	while (true) {
+		char answer[200];
+		// _getch()
+		cin.getline(answer, 199);
+		int iSendResult = send(ClientSocket, answer, strlen(answer), 0); // The send function sends data on a connected socket: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-send
+
+		if (iSendResult == SOCKET_ERROR) {
+			cout << "send failed with error: " << WSAGetLastError() << "\n";
+			cout << "упс, отправка (send) ответного сообщения не состоялась ((\n";
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 7;
+		}
+	}
+
+	return 0;
+}
+
+DWORD WINAPI Receiver(void* param)
+{
+	while (true) {
+		char message[DEFAULT_BUFLEN];
+
+		int iResult = recv(ClientSocket, message, DEFAULT_BUFLEN, 0); // The recv function is used to read incoming data: https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-recv
+		message[iResult] = '\0';
+
+		if (iResult > 0)
+			cout << message << "\n"; // 1 або 2
+	}
+	return 0;
+}
+
+int main()
+{
+	setlocale(0, "");
+	system("title SERVER SIDE");
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Initialize Winsock
+	WSADATA wsaData; // The WSADATA structure contains information about the Windows Sockets implementation: https://docs.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-wsadata
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // The WSAStartup function initiates use of the Winsock DLL by a process: https://firststeps.ru/mfc/net/socket/r.php?2
+	if (iResult != 0) {
+		cout << "WSAStartup failed with error: " << iResult << "\n";
+		cout << "подключение Winsock.dll прошло с ошибкой!\n";
+		return 1;
+	}
+	else {
+		// cout << "подключение Winsock.dll прошло успешно!\n";
+		Sleep(PAUSE);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	struct addrinfo hints; // The addrinfo structure is used by the getaddrinfo function to hold host address information: https://docs.microsoft.com/en-us/windows/win32/api/ws2def/ns-ws2def-addrinfoa
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET; // The Internet Protocol version 4 (IPv4) address family
+	hints.ai_socktype = SOCK_STREAM; // Provides sequenced, reliable, two-way, connection-based byte streams with a data transmission mechanism
+	hints.ai_protocol = IPPROTO_TCP; // The Transmission Control Protocol (TCP). This is a possible value when the ai_family member is AF_INET or AF_INET6 and the ai_socktype member is SOCK_STREAM
+	hints.ai_flags = AI_PASSIVE; // The socket address will be used in a call to the "bind" function
+
+	// Resolve the server address and port
+	struct addrinfo* result = NULL;
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		cout << "getaddrinfo failed with error: " << iResult << "\n";
+		cout << "получение адреса и порта сервера прошло c ошибкой!\n";
+		WSACleanup(); // The WSACleanup function terminates use of the Winsock 2 DLL (Ws2_32.dll): https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsacleanup
+		return 2;
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Create a SOCKET for connecting to server
+	SOCKET ListenSocket = INVALID_SOCKET;
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		cout << "socket failed with error: " << WSAGetLastError() << "\n";
+		cout << "создание сокета прошло c ошибкой!\n";
+		freeaddrinfo(result);
+		WSACleanup();
+
+		return 3;
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen); // The bind function associates a local address with a socket: https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-bind
+	if (iResult == SOCKET_ERROR) {
+		cout << "bind failed with error: " << WSAGetLastError() << "\n";
+		cout << "внедрение сокета по IP-адресу прошло с ошибкой!\n";
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 4;
+	}
+
+	freeaddrinfo(result);
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	iResult = listen(ListenSocket, SOMAXCONN); // The listen function places a socket in a state in which it is listening for an incoming connection: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-listen
+	if (iResult == SOCKET_ERROR) {
+		cout << "listen failed with error: " << WSAGetLastError() << "\n";
+		cout << "прослушка информации от клиента не началась. что-то пошло не так!\n";
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 5;
+	}
+	else {
+		cout << "пожалуйста, запустите client.exe\n";
+		// здесь можно было бы запустить некий прелоадер в отдельном потоке
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	ClientSocket = accept(ListenSocket, NULL, NULL); // The accept function permits an incoming connection attempt on a socket: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-accept
+	if (ClientSocket == INVALID_SOCKET) {
+		cout << "accept failed with error: " << WSAGetLastError() << "\n";
+		cout << "соединение с клиентским приложением не установлено! печаль!\n";
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 6;
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	CreateThread(0, 0, Receiver, 0, 0, 0);
+
+	CreateThread(0, 0, Sender, 0, 0, 0);
+
+	Sleep(INFINITE);
+}
